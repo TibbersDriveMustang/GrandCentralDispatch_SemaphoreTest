fff029f451f96a22b5049b3cf5856e2555dabcbb
Hongyi Guo <hongyi.guo@utdallas.edu>
2015-10-02 08:30:31 -0500

    Initial Commit

---
+++ semaphoreTEST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  semaphoreTEST
+//
+//  Created by Hongyi Guo on 10/2/15.
+//  Copyright © 2015 Hongyi Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
